fb3cc099aaa71f83bb6d9b13e5ba4fb1400176df
tylerguy <tylerdenman200@gmail.com>
2023-05-30 19:09:02 +0100

    Make trigger and collider seperate gameobjects

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -7,10 +7,14 @@ public class GameState : MonoBehaviour
 {
     // Start is called before the first frame update
     public Camera cam;
+    public GameObject middleTrigger;
     public GameObject middleCollider;
+    private GameObject player;
+    private GameObject boss;
     void Start()
     {
-
+        player = GameObject.Find("Player");
+        boss = GameObject.Find("Boss");
     }
 
     // Update is called once per frame
@@ -44,15 +48,7 @@ public class GameState : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             cam.transform.position = new Vector3(3, 5, -29.5f);
-            // wait for 1 second
-            StartCoroutine(Wait());
+            middleCollider.SetActive(true);
         }
-    }
-
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(2);
-        middleCollider.GetComponent<BoxCollider2D>().isTrigger = false;
-
     }
 }
